5fedbe2e6f789fffe1e0ffd091b403ce02aa49a2
azummo-google@towertech.it <azummo-google@towertech.it@3c61013c-4947-0410-906f-bb40884d2d27>
2008-03-03 18:00:42 +0000

    fixed indentation

--- trunk/include/md5.h
+++ trunk/include/md5.h
@@ -55,17 +55,17 @@ typedef uintptr_t md5_uintptr;
 # endif
 
 # if UINT_MAX == UINT_MAX_32_BITS
-   typedef unsigned int md5_uint32;
+typedef unsigned int md5_uint32;
 # else
 #  if USHRT_MAX == UINT_MAX_32_BITS
-    typedef unsigned short md5_uint32;
+typedef unsigned short md5_uint32;
 #  else
 #   if ULONG_MAX == UINT_MAX_32_BITS
-     typedef unsigned long md5_uint32;
+typedef unsigned long md5_uint32;
 #   else
      /* The following line is intended to evoke an error.
         Using #error is not portable enough.  */
-     "Cannot determine unsigned 32-bit data type."
+"Cannot determine unsigned 32-bit data type."
 #   endif
 #  endif
 # endif
@@ -87,19 +87,19 @@ typedef unsigned long int md5_uintptr;
 #define ALIGN __attribute__ ((__aligned__ (4)))
 #else
 #define ALIGN
-#endif 
+#endif
 
 /* Structure to save state of computation between the single steps.  */
 struct md5_ctx
 {
-  md5_uint32 A;
-  md5_uint32 B;
-  md5_uint32 C;
-  md5_uint32 D;
+	md5_uint32 A;
+	md5_uint32 B;
+	md5_uint32 C;
+	md5_uint32 D;
 
-  md5_uint32 total[2];
-  md5_uint32 buflen;
-  char buffer[128] ALIGN;
+	md5_uint32 total[2];
+	md5_uint32 buflen;
+	char buffer[128] ALIGN;
 };
 
 /*
@@ -109,21 +109,21 @@ struct md5_ctx
 
 /* Initialize structure containing state of computation.
    (RFC 1321, 3.3: Step 3)  */
-extern void __md5_init_ctx __P ((struct md5_ctx *ctx));
+extern void __md5_init_ctx __P((struct md5_ctx * ctx));
 
 /* Starting with the result of former calls of this function (or the
    initialization function update the context for the next LEN bytes
    starting at BUFFER.
    It is necessary that LEN is a multiple of 64!!! */
-extern void __md5_process_block __P ((const void *buffer, size_t len,
-				      struct md5_ctx *ctx));
+extern void __md5_process_block __P((const void *buffer, size_t len,
+				     struct md5_ctx * ctx));
 
 /* Starting with the result of former calls of this function (or the
    initialization function update the context for the next LEN bytes
    starting at BUFFER.
    It is NOT required that LEN is a multiple of 64.  */
-extern void __md5_process_bytes __P ((const void *buffer, size_t len,
-				      struct md5_ctx *ctx));
+extern void __md5_process_bytes __P((const void *buffer, size_t len,
+				     struct md5_ctx * ctx));
 
 /* Process the remaining bytes in the buffer and put result from CTX
    in first 16 bytes following RESBUF.  The result is always in little
@@ -132,7 +132,7 @@ extern void __md5_process_bytes __P ((const void *buffer, size_t len,
 
    IMPORTANT: On some systems it is required that RESBUF is correctly
    aligned for a 32 bits value.  */
-extern void *__md5_finish_ctx __P ((struct md5_ctx *ctx, void *resbuf));
+extern void *__md5_finish_ctx __P((struct md5_ctx * ctx, void *resbuf));
 
 
 /* Put result from CTX in first 16 bytes following RESBUF.  The result is
@@ -141,19 +141,19 @@ extern void *__md5_finish_ctx __P ((struct md5_ctx *ctx, void *resbuf));
 
    IMPORTANT: On some systems it is required that RESBUF is correctly
    aligned for a 32 bits value.  */
-extern void *__md5_read_ctx __P ((const struct md5_ctx *ctx, void *resbuf));
+extern void *__md5_read_ctx __P((const struct md5_ctx * ctx, void *resbuf));
 
 
 /* Compute MD5 message digest for bytes read from STREAM.  The
    resulting message digest number will be written into the 16 bytes
    beginning at RESBLOCK.  */
-extern int __md5_stream __P ((FILE *stream, void *resblock));
+extern int __md5_stream __P((FILE * stream, void *resblock));
 
 /* Compute MD5 message digest for LEN bytes beginning at BUFFER.  The
    result is always in little endian byte order, so that a byte-wise
    output yields to the wanted ASCII representation of the message
    digest.  */
-extern void *__md5_buffer __P ((const char *buffer, size_t len,
-				void *resblock));
+extern void *__md5_buffer __P((const char *buffer, size_t len,
+			       void *resblock));
 
 #endif /* md5.h */

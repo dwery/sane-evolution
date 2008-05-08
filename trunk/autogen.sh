0fcfc2151fe3f30ac26093fd6c450519831cdc9d
azummo-google@towertech.it <azummo-google@towertech.it@3c61013c-4947-0410-906f-bb40884d2d27>
2008-05-08 14:54:41 +0000

    do not remove mkinstalldirs

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -9,7 +9,7 @@ set -e
 # Refresh GNU autotools toolchain.
 echo Cleaning autotools files...
 find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
-find -type f \( -name missing -o -name mkinstalldirs \
+find -type f \( -name missing \
 	-o -name depcomp -o -name ltmain.sh -o -name configure \
 	-o -name config.sub -o -name config.guess \
 	\) -print0 | xargs -0 rm -f
